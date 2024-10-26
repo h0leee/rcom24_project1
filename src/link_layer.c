01bd57bd0f075562921ef40b2d049dd2aa1b1271
Rodrigo Resende <93944082+Resende16@users.noreply.github.com>
2024-10-26 21:09:37 +0100

    LLWRITE done and commented

--- src/link_layer.c
+++ src/link_layer.c
@@ -17,6 +17,12 @@ void alarmHandler(int signal)
 
 volatile int STOP = FALSE;
 
+
+
+
+
+
+
 ////////////////////////////////////////////////
 // LLOPEN
 ////////////////////////////////////////////////
@@ -123,15 +129,186 @@ int llopen(LinkLayer connectionParameters)
 }
 
 
+
+
+
+/////////////////////////////////////////////////////
+//STUFFING
+/////////////////////////////////////////////////////
+
+//Stuffing e o processo inverso vao ser usados no LLWRITE
+
+int byteStuffing(const unsigned char *inputMsg, int inputSize, unsigned char *outputMsg)
+{
+    int stuffedSize = 0; //Variável para guardar o tamanho da mensagem (quantos bytes foram escritos em outputMsg)
+
+    outputMsg[stuffedSize++] = inputMsg[0]; //o stuffedSize serve como indice para avançar para a próxima posição
+
+    printf("\nSTUFFING STARTED\n"); //debug
+
+    printf("%x\n", outputMsg[stuffedSize - 1]); 
+
+    for (int i = 1; i < inputSize; i++)
+    {
+        if (inputMsg[i] == FLAG || inputMsg[i] == ESCAPE) //bytes de controlo, se for igual é necessário stuffing
+        {
+            outputMsg[stuffedSize++] = ESCAPE; //indica que o próximo byte foi modificado
+            outputMsg[stuffedSize++] = inputMsg[i] ^ 0x20; 
+
+        }
+        else
+        {
+            //se não for FLAG ou ESCAPE vai copiar o byte logo
+            outputMsg[stuffedSize++] = inputMsg[i];
+
+        }
+    }
+
+    printf("\nSTUFFING COMPLETED\n"); //debug
+
+    return stuffedSize;
+}
+
+
+// função usada para reverter o byte stuffing aplicado na mensagem
+
+int byteDestuffing(const unsigned char *stuffedMsg, int stuffedSize, unsigned char *originalMsg)
+{
+    int destuffedSize = 0; // vai contar quantos bytes sao escritos
+
+    printf("\nDESTUFFING STARTED\n"); // debug
+
+    originalMsg[destuffedSize++] = stuffedMsg[0]; 
+
+    for (int i = 1; i < stuffedSize; i++)
+    {
+        if (stuffedMsg[i] == ESCAPE) // vemos se o byte foi mudificado com stuffing para saber se temos de restaurar
+        {
+            originalMsg[destuffedSize++] = stuffedMsg[i + 1] ^ 0x20; // byte é restaurado e incrementamos para a posição seguinte
+            i++;
+        }
+        else
+        {
+            originalMsg[destuffedSize++] = stuffedMsg[i]; // se nao for ESCAPE copia logo
+        }
+    }
+
+    printf("\nDESTUFFING COMPLETED\n"); // debug
+
+    return destuffedSize;
+}
+
+
+// BCC -> block check character
+
+unsigned char computeBCC2(const unsigned char *buffer, int length, int startByte)
+{
+    if (length < 0) // se o tamanho do buffer for negativo como é obvio retornará erro mas sem interromper a execução
+    {
+        printf("Error: Buffer size is %d\n", length);
+    }
+    unsigned char BCC2Value = 0x00; // inicializamos a zero para fazer o XOR
+
+    for (unsigned int i = startByte; i < length; i++) // este loop vaipercorrer o buffer
+    {
+        BCC2Value ^= buffer[i]; // ^= é o XOR que é aplicado ao BCC2
+    }
+    return BCC2Value;
+}
+
+
+
+
+
+
+
 ////////////////////////////////////////////////
 // LLWRITE
 ////////////////////////////////////////////////
+
 int llwrite(const unsigned char *buf, int bufSize)
 {
-    // TODO
+    int totalSize = bufSize + 5; // tamanho total do frame
+
+    unsigned char frame[totalSize]; // variavel para armazenar o frame antes do stuffing
+
+    static int frameSequence = 0; 
+
+    frame[0] = FLAG;
+    frame[1] = A_T; // byte de endereço
+    frame[2] = C_INF(frameSequence); // byte de controlo
+    frame[3] = BCC(A_T, C_INF(frameSequence)); // BCC calculado entre o A e o C
+
+    unsigned char calculatedBCC2 = buf[0]; 
+
+    for (int i = 0; i < bufSize; i++)
+    {
+        frame[i + 4] = buf[i];
+        if (i > 0)
+            calculatedBCC2 ^= buf[i]; // é usado o XOR novamente para calcular o BCC2
+    }
+
+    frame[bufSize + 4] = calculatedBCC2; // é inserido no final do frame
+
+    unsigned char stuffedFrame[totalSize * 2]; // vai armazenar o frame depois do stuffing
+
+    // aqui vamos aplicar o stuffing ao frame e copiamos para o stuffedframe
+    totalSize = byteStuffing(frame, totalSize, stuffedFrame); 
+    stuffedFrame[totalSize] = FLAG; // a flag assinala o fim do processo
+    totalSize++;
+
+
+    //inicisalização das variaveis para controlar o alarme e state machine
+    STOP = FALSE; 
+    alarmEnabled = FALSE;
+    alarmCount = 0;
+    state = START;
+
+    // aqui contamos o nr de tentativas e temos rejected para ver se o frame é rejeitado ou nao
+    int attemptCount = 0;
+    int rejected = FALSE;
+
+
+    while (!STOP && alarmCount < layer.nRetransmissions) // while até o stop ser true ou seja ate o frame ser aceite
+    {
+
+        // se o alarmenabled for false vai fazer uma tentativa de envio
+        if (!alarmEnabled) 
+        {
+            attemptCount++;
+            bytes = write(fd, stuffedFrame, totalSize); // aqui enviamos ao usar o write
+            alarm(layer.timeout);
+            alarmEnabled = TRUE;
+            state = START; // start para iniciar a state machine
+        }
+
+        stateMachine(A_T, NULL, 0, 1); // chamamos a state machine de cima
+
+        if ((frameSequence == 0 && response == REJ1) || (frameSequence == 1 && response == REJ0)) // verificamos se o frame foi ou nao rejeitado, se REJ1 entao rejected = true
+        {
+            rejected = TRUE;
+        }
+
+        if (rejected) // se foi rejeitado desativa o alarme para tentar enviar outra vez
+        {
+            alarm(0);
+            alarmEnabled = FALSE;
+        }
+    }
+
+    frameSequence = (frameSequence + 1) % 2;
+
+    alarm(0); //alarme é desligado depois de ter conseguido enviar
+
+    printf("Data Successfully Accepted!\n"); //debug
 
     return 0;
 }
+
+
+
+
+
 
 ////////////////////////////////////////////////
 // LLREAD
