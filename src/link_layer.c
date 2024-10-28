6dea8f92a66dcafd74a7a532dbb4e7a9d7556f12
Rodrigo Miranda <rucamir2004@gmail.com>
2024-10-28 22:01:35 +0000

    some changes linkLayer

--- src/link_layer.c
+++ src/link_layer.c
@@ -1,10 +1,29 @@
 // Link layer protocol implementation
 
+
+// depois poderei tentar modular o código da stateMachine 
+
 #include "link_layer.h"
 #include "serial_port.h"
 
+#define A_RE = 0x01
+
+
+volatile int STOP = FALSE;
 int alarmEnabled = FALSE;
 int alarmCount = 0;
+int timeOut = 0;
+int retransmissions = 0;
+unsigned char tramaTx = 0;
+unsigned char tramaRx = 1;
+
+
+
+typedef enum {
+    START, A, C, F, STOP, BCC1, BCC2, ESC, READING_PAYLOAD
+} LinkLayerState;
+
+
 
 // Alarm function handler
 void alarmHandler(int signal)
@@ -15,12 +34,43 @@ void alarmHandler(int signal)
     printf("Alarm #%d\n", alarmCount);
 }
 
-volatile int STOP = FALSE;
+
+int connection(const char *serialPort) {
+
+    int fd = connection(serialPort);
+    if (fd < 0) {
+        perror(serialPort);
+        return -1; 
+    }
 
 
+    struct termios oldtio;
+    struct termios newtio;
 
+    if (tcgetattr(fd, &oldtio) == -1)
+    {
+        perror("tcgetattr");
+        exit(-1);
+    }
 
+    memset(&newtio, 0, sizeof(newtio));
 
+    newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
+    newtio.c_iflag = IGNPAR;
+    newtio.c_oflag = 0;
+    newtio.c_lflag = 0;
+    newtio.c_cc[VTIME] = 0;
+    newtio.c_cc[VMIN] = 0;
+
+    tcflush(fd, TCIOFLUSH);
+
+    if (tcsetattr(fd, TCSANOW, &newtio) == -1) {
+        perror("tcsetattr");
+        return -1;
+    }
+
+    return fd;
+}
 
 
 ////////////////////////////////////////////////
@@ -28,14 +78,10 @@ volatile int STOP = FALSE;
 ////////////////////////////////////////////////
 int llopen(LinkLayer connectionParameters)
 {
-    if (openSerialPort(connectionParameters.serialPort,
-                       connectionParameters.baudRate) < 0)
+    if (openSerialPort(connectionParameters.serialPort, connectionParameters.baudRate) < 0)
     {
         return -1;
     }
-
-    layer = connectionParameters; //Criamos uma variável layer para guardar o valor do connectionParameters
-    (void)signal(SIGALRM, alarmHandler); //Quando o sinal SIGALRM for gerado (por exemplo, após o uso de alarm()), a função alarmHandler será chamada.
 
     // (Semelhante ao writenoncanonical.c)
 
@@ -47,82 +93,58 @@ int llopen(LinkLayer connectionParameters)
         perror(connectionParameters.serialPort);
         exit(-1);
     }
-
-    //Estruturas usadas para configurar parâmetros da porta de série.
-
-    struct termios oldtio; //oldtio armazena a configuração atual da porta de série.
-    struct termios newtio; //newtio armazena a nova configuração da porta de série.
-
-    //Guardar as definições da porta de série.
-
-    if(tcgetattr(fd, &oldtio) == -1){
-        perror("tcgetattr");
-        exit(-1);
-    }
-
-    //Limpar a struct para armazenar as novas configurações da porta de série.
-
-    memset(&newtio, 0, sizeof(newtio));
-
-    newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
-    newtio.c_iflag = IGNPAR; //Ignora erros de paridade ao receber dados.
-    newtio.c_oflag = 0; 
-
-    newtio.c_lflag = 0; //Configura a comunicação em modo noncanonical
-    newtio.c_cc[VTIME] = 0; 
-    newtio.c_cc[VMIN] = 0;  //A função read só dá return depois de serem recebidos pelo menos 0 caracteres.
-
-    tcflush(fd, TCIOFLUSH);
-
-    //Definir novas configurações da porta de série:
-
-    if(tcsetattr(fd, TCSANOW, &newtio) == -1){
-        perror("tcsetattr");
-        exit(-1);
-    }
-
     printf("New termio structure set\n");
 
+    int alarmTriggered = FALSE;
+    LinkLayerState machineState = START;
     sleep(1); // O sleep espera até que todos os bytes tenham sido escritos
+    unsigned char byteRead;
+    int timeOut = connectionParameters.timeout;
+    int retransmissions = connectionParameters.nRetransmissions;
 
-    STOP = FALSE; //STOP é usado como uma flag para depois terminar o loop em baixo
-    int bytes = 0; //Variável para armazenar os bytes enviados ou recebidos
+    switch (connectionParameters.role) {
+        case LlTx: {
+                // Quando o sinal SIGALRM for acionado, a função alarmHandler será chamada.
+                (void) signal(SIGALRM, alarmHandler); 
 
-    if (connectionParameters.role == LlTx) //Verifica se é transmissor
-    {
-        while (!STOP && alarmCount < connectionParameters.nRetransmissions)
-        {
-            if (!alarmEnabled)
-            {
-                bytes = sendBuffer(A_T, C_SET); //SET para iniciar a comunicação e armazenar o nr de bytes enviados na variavel bytes
-                printf("%d bytes written\n", bytes);
+                while (machineState != STOP && !alarmTriggered) {
 
-                alarm(connectionParameters.timeout);
-                alarmEnabled = TRUE; 
-                state = START;
-            }
-            stateMachine(A_T, C_UA, 0, 0); //A state machine aguarda a resposta UA do recetor
+                    if(read(fd, &byteRead, 1) > 0) {
+                        switch(machineState) {
+                            case START:
+                                if (byteRead == FLAG) machineState = F;
+                                break;
+                            
+                            case F:
+                                if(byteRead == 0x01) machineState = A;
+                                break;
+
+                            case A:
+                                if(byteRead == C_RE) machineState = C;
+                                break;
+
+                            case C:
+                                if(byteRead == BCC1) machineState = BCC1;
+                                break;
+
+                            case BCC1:
+                                if(byteRead == FLAG) machineState = STO;
+                                break;
+
+                            default:
+                                break;
+                        }
+                    }
+
+                }
+                break; // Adicionado break para evitar fall-through
         }
-        alarm(0); // Caso o transmissor tenha recebido a resposta UA, desativa o alarme
 
-        if (alarmCount >= layer.nRetransmissions) //Se o nr de trasmissoes atingiu o limite (alarmCount), o transmissor nao conseguiu estabelecer ligação por isso: Erro TimeOut
-        {
-            printf("ERRO TIME OUT\n");
-            return -1;
+        
+        case LlRx: {
+            // Implementação para LlRx
+            break; // Adicionado break para evitar fall-through
         }
-
-        printf("LLOPEN OK\n");
-    }
-
-    else if (connectionParameters.role == LlRx) //Aqui vai ser o recetor a aguardar o SET do transmissor
-    {
-        while (!STOP) //Espera pelo comando SET para entrar no loop
-        {
-            stateMachine(A_T, C_SET, 0, 0); 
-        }
-
-        bytes = sendBuffer(A_T, C_UA); //Quando recetor recebe SET envia de volta a resposta UA para o transmissor para avisar que está tudo ok na transmissao
-        printf("RESPONSE TO LLOPEN TRANSMITTER. %d bytes written\n", bytes);
     }
 
     return 0;
